b5ee0f2b137fe48695250be535277ee2bb6e0de3
Mark Lester Bolotaolo <mlab817@gmail.com>
2021-11-04 09:46:27 +0800

    fixed logo attribute

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -62,8 +62,8 @@ class Office extends Model
     public function getLogoAttribute(): ?string
     {
         return $this->operating_unit
-            ? public_path('images') . '/'. strtoupper($this->operating_unit->acronym) . '.png'
-            : public_path('images/DA-CO.png');
+            ? public_path('images') . '/offices/'. strtoupper($this->operating_unit->acronym) . '.png'
+            : public_path('images/offices/DA-CO.png');
     }
 
     /**
