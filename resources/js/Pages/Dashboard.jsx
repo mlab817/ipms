8023d82298384847aaace70aec1bb71a622d6e93
Lester Bolotaolo <ipd-lester@Lesters-iMac.local>
2022-08-05 08:54:08 +0800

    added charts

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import Authenticated from '@/Layouts/Authenticated';
 import { Head } from '@inertiajs/inertia-react';
+import {Box} from "@primer/react";
+import {LineSeries, VerticalBarSeries, XYPlot} from "react-vis";
+
+const data = [
+    {x: 0, y: 8},
+    {x: 1, y: 5},
+    {x: 2, y: 4},
+    {x: 3, y: 9},
+    {x: 4, y: 1},
+    {x: 5, y: 7},
+    {x: 6, y: 6},
+    {x: 7, y: 3},
+    {x: 8, y: 2},
+    {x: 9, y: 0}
+];
 
 export default function Dashboard(props) {
     return (
@@ -9,7 +24,7 @@ export default function Dashboard(props) {
             errors={props.errors}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
         >
-            <Head title="Dashboard" />
+            <Head title="Dashboard"><title>Dashboard</title></Head>
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -18,6 +33,19 @@ export default function Dashboard(props) {
                     </div>
                 </div>
             </div>
+
+            <Box display="flex">
+                <Box flex={1}>
+                    <XYPlot height={600} width={600}>
+                        <VerticalBarSeries data={data} />
+                    </XYPlot>
+                </Box>
+                <Box flex={1}>
+                    <XYPlot height={200} width={200}>
+                        <LineSeries data={data} />
+                    </XYPlot>
+                </Box>
+            </Box>
         </Authenticated>
     );
 }
--- resources/views/layouts/default.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-// This is a persistent layout
-// Include me with <template layout="default" />
-</script>
-
-<template>
-	<slot />
-</template>
